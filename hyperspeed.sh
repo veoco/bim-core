f91a3dd2c461cf65f22d039d2c95dc599ce30ee6
veoco <one@nomox.cn>
2022-09-20 21:34:06 +0800

    move B

--- hyperspeed.sh
+++ hyperspeed.sh
@@ -73,7 +73,7 @@ speed_test(){
     local jitter=$(echo $output | cut -d ',' -f4)
             
     if [[ $(awk -v num1=${upload} -v num2=0.0 'BEGIN{print(num1>num2)?"1":"0"}') -eq 1 ]]; then
-        printf "\r${RED}B%-6s${YELLOW}%s%s${GREEN}%s${CYAN}%s%-10s${BLUE}%s%-10s${GREEN}%-10s${PURPLE}%-6s${ENDC}\n" "${nodeID}"  "${nodeISP}" "|" "${name}" "↑ " "${upload}" "↓ " "${download}" "${latency}" "${jitter}"
+        printf "\rb${RED}%-6s${YELLOW}%s%s${GREEN}%s${CYAN}%s%-10s${BLUE}%s%-10s${GREEN}%-10s${PURPLE}%-6s${ENDC}\n" "${nodeID}"  "${nodeISP}" "|" "${name}" "↑ " "${upload}" "↓ " "${download}" "${latency}" "${jitter}"
     fi
 
 }
